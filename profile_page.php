ec94b06fd368dc5cd64596fe7d8d9f47255c57b6
rodrichulo <rodri100172@gmail.com>
2023-04-21 00:44:31 -0400

    more changes to profile page

--- profile_page.php
+++ profile_page.php
@@ -10,6 +10,14 @@
 <html>
 
 <head>
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="style.css" />
 	<title>My Profile Page</title>
 	<style>
 		body {
@@ -182,6 +190,41 @@
 </head>
 
 <body>
+	<nav>
+		<div class="logo">
+		<h2><a href="index.html" style = "text-decoration:none; 
+		color:rgb(255, 254, 254); font-size:35px;">
+		GymPal</a></h2>
+		</div>
+
+	<ul>
+	<li><a href="index.html">Home</a></li>
+	<li><a href="about_us.html">About</a></li>
+	<li><a href="profile_page.php">About</a></li>
+	<li><a href="exercises.html">Workouts </a></li>
+	<li><a href="contact.html">Contact</a></li>
+	<li><a href="signup.html">Sign up/Login</a></li>
+	</ul>
+	<div class="menu-bars">
+	<input type="checkbox">
+	<span></span>
+	<span></span>
+	<span></span>
+	</div>
+	</nav>
+
+
+	<script>
+	const menuToggle= document.querySelector(".menu-bars");
+	const nav = document.querySelector("nav ul");
+
+
+	menuToggle.addEventListener("click", () => {
+	nav.classList.toggle("slide");
+	});
+	</script>
+
+	
 	<div class="container">
 		<img class="profile-picture" src="https://via.placeholder.com/200x200" alt="Profile Picture">
 		<div class="profile-info">
