3ea0c16ae3846065303b60242bc2772029ddd803
Adam-Weiss8 <aweiss008@gmail.com>
2023-04-24 21:36:25 -0400

    changed database push workouts

--- processWorkout.php
+++ processWorkout.php
@@ -30,16 +30,16 @@ $WorkoutID = $row[0];
 //add all exercises to the exercise table
 for ($i = 0; $i < $numExercises; $i++) {
         //prep post strings
-        $exerciseNumString = "exerciseName" . $i;
-        $setsString = "sets" . $i;
-        $repsString = "reps" . $i;
-        $weightString = "weight" . $i;
+        $exerciseNumString = "exerciseName";
+        $setsString = "sets";
+        $repsString = "reps";
+        $weightString = "weight";
 
         //get form data
-        $exerciseNum = $_POST[$exerciseNumString];
-        $sets = $_POST[$setsString];
-        $reps = $_POST[$repsString];
-        $weight = $_POST[$weightString];
+        $exerciseNum = $_POST[$exerciseNumString][$i];
+        $sets = $_POST[$setsString][$i];
+        $reps = $_POST[$repsString][$i];
+        $weight = $_POST[$weightString][$i];
 
         //add data to the database
         $sql = "INSERT INTO exercises (exerciseName, workoutID, reps, sets, weight) VALUES ('$exerciseNum', '$WorkoutID', '$sets', '$reps', '$weight')";
